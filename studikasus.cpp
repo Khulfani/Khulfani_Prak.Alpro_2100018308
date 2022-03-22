e5ff427536afda5624ace7ff28c89b073fefd54c
Khulfani <khulfani1@gmail.com>
2022-03-22 19:30:20 +0700

    Studi kasus-cpp

---
+++ studikasus.cpp
@@ -0,0 +1,18 @@
+//BAGIAN 1
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+using namespace std;
+float pertalite(float x){
+	float hasil;
+	return hasil = x * 7000 ;
+}
+float pertamax(float x){
+	float hasil;
+	return hasil = x * 9000;
+}
+int main(){
+	string nama,bensin;
+	int pil,hrg;
+	float liter,harga;
+}
