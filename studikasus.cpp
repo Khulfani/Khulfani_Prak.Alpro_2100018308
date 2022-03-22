e4cf15d962f6d777d52a397d381bd01a756fd1b8
rajuputra <raju2100018316@webmail.uad.ac.id>
2022-03-22 19:36:02 +0700

    update 1 studikasus-cpp

--- studikasus.cpp
+++ studikasus.cpp
@@ -15,4 +15,24 @@ int main(){
 	string nama,bensin;
 	int pil,hrg;
 	float liter,harga;
+	cout << "Program SPBU\n";
+	cout << "Masukkan nama anda : "; cin  >> nama;
+	cout << "Jenis bensin :\n";
+	cout << "1.Pertalite \n2.Pertamax\n";
+	cout << "Masukkan pilihan jenis bensin : ";
+	cin >> pil;
+	if (pil == 1){
+		bensin = "Pertalite";
+		hrg = 7000;
+		cout << "Masukkan jumlah liter : " ;
+		cin >> liter;
+		harga = pertalite(liter);
+	}
+	else if (pil == 2){
+		bensin = "Pertamax";
+		hrg = 9000;
+		cout << "Masukkan jumlah liter : " ;
+		cin >> liter;
+		harga = pertamax(liter);
+	}
 }
